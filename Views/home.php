1dcfe33096fe695fd01ad2bb4cc2334d9f584ad0
Seb27 <seb-roche@orange.fr>
2019-04-04 21:46:17 +0200

    added the home page content

--- Views/home.php
+++ Views/home.php
@@ -1,6 +1,14 @@
 <?php
 
 $title = 'Billet simple pour l\'Alaska - Accueil';
-$content = '<p>Bienvenue sur home</p>';
-
 require('template.php');
+
+?>
+
+<p>Qu'est ce que billet simple pour l'Alaska ?</p>
+<p>Billet simple pour l'alaska est un roman publié en ligne et dans lequel votre serviteur vous présentera les aventures de Jean Martin de manière épisodique.</p>
+<div>
+    <a href="chapter.php?chapter=1">Lire le premier chapitre</a>
+    <a href="about.php">Découvrir votre serviteur</a>
+</div>
+
